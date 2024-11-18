6e96aaf819543ab8b7584392954f92bb861a629c
Ruan de Moraes Alexandre <mruan309@gmail.com>
2024-11-18 20:09:09 -0300

    deu tudo errado
    voltando alterações como antes

--- sistema/fornecedores/fornecedores_listar.php
+++
@@ -1,5 +0,0 @@
-<?php require_once $_SERVER['DOCUMENT_ROOT'] . "/garagem-itech/header_sistema.php"?>
-    <div>
-        <h1>bateu nos fornecedores</h1>
-    </div>
-<?php require_once $_SERVER['DOCUMENT_ROOT'] . "/garagem-itech/footer_sistema.php" ?>
--- sistema/vendas/vendas_listar.php
+++
@@ -1,5 +0,0 @@
-<?php require_once $_SERVER['DOCUMENT_ROOT'] . "/garagem-itech/header_sistema.php"?>
-    <div>
-        <h1>bateu nas vendas</h1>
-    </div>
-<?php require_once $_SERVER['DOCUMENT_ROOT'] . "/garagem-itech/footer_sistema.php" ?>
